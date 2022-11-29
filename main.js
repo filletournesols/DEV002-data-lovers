a3a7550c19f06fb5c24e5c8acadb8ed642a9e237
filletournesols <dgalarcegonzalez@gmail.com>
2022-11-29 14:19:15 -0300

    Updates

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-import {getAllCharacters} from "./data.js";
+import { getAllCharacters, filterCharactersFun, sortingCharacters } from "./data.js";
 
 // manejo inputs
 const inputs = document.querySelectorAll(".inputSearch");
+const searchButtons = document.querySelectorAll("#searchButtonNav, #searchButtonHome");
+
 const createCharactersHtml = (characters) =>{
   let characterName = "";
   characters.forEach(character => {
     characterName +=
     `<div class="character">
       <img class="character-image" src="${character.image || "assets/user.png"}"/>
-      <h3 class="name">${character.name}</h3>
+      <button class="name">${character.name}</button>
     </div>`
   })
   return characterName
 }
+
 const characterNotFound = () => {
   let characterDataNotFound =
     `<div class="characterNotFound">
@@ -24,13 +27,13 @@ const characterNotFound = () => {
 inputs.forEach(input =>{
   input.addEventListener("keydown", async (event) =>{
     if (event.keyCode === 13){
-
       /* Reseting sort options */
       const sortButtons = document.getElementsByName('sort-characters');
       sortButtons.forEach(button => button.checked = false);
 
       document.getElementById("home").hidden = true;
       document.getElementById("navSearcher").classList.remove("inactive");
+
       //data contiene el resultado de la promesa
       let data = await getAllCharacters();
       //filter(función de arreglo) necesita una condición que se entrega con return. la condición entrega true o false
@@ -44,23 +47,115 @@ inputs.forEach(input =>{
         window.localStorage.setItem('Character', JSON.stringify(result));
         const charactersHTML = createCharactersHtml(result);
         document.getElementById("results").innerHTML = charactersHTML
+        showCharacterDetails()
       }
     }
   })
 })
 
-// esconder los ... y mostrar characters
+searchButtons.forEach(btn =>{
+  btn.addEventListener("click", async () => {
+    const inputValue = document.getElementById("home").hidden ? inputs[1].value : inputs[0].value
+    /* Reseting sort options */
+    const sortButtons = document.getElementsByName('sort-characters');
+    sortButtons.forEach(button => button.checked = false);
+
+    document.getElementById("home").hidden = true;
+    document.getElementById("navSearcher").classList.remove("inactive");
+    //data contiene el resultado de la promesa
+    let data = await getAllCharacters();
+    //filter(función de arreglo) necesita una condición que se entrega con return. la condición entrega true o false
+    let result = data.filter((characters) => {
+      return characters.name.toLowerCase().includes(inputValue.toLowerCase())
+    })
+    if (result == false){
+      const dataNotFound = characterNotFound(result);
+      document.getElementById("results").innerHTML = dataNotFound
+    } else {
+      window.localStorage.setItem('Character', JSON.stringify(result));
+      const charactersHTML = createCharactersHtml(result);
+      document.getElementById("results").innerHTML = charactersHTML
+    }
+  })
+})
+
+// esconder los ... y mostrar characters y status
 const dotButton = document.getElementById("dotsButton");
 
 if (dotButton){
   dotButton.addEventListener("click", () => {
     document.getElementById("dotsButton").hidden = true;
     document.getElementById("charactersButton").classList.remove("inactive");
+    document.getElementById("statusButton").classList.remove("inactive");
   })
 }
 
 // al hacer click en characters, se esconde home y se muestra el nav con los personajes (nombre e imagen)
 const characterButton = document.getElementById("charactersButton");
+
+/* character details */
+function showCharacterDetails(){
+  const characterDetailsButton = document.querySelectorAll(".name");
+  const searchResultsContainer = document.querySelector("#searchResults");
+  const characterDetailsContainer = document.querySelector('.characters-details')
+  const detailsContainer = document.querySelector(".details")
+  const closeDetailsContainer = document.querySelector("#close-details")
+
+  characterDetailsButton.forEach(button => {
+    button.onclick = (e) => {
+      const characterName = e.target.innerText
+      const characterList = JSON.parse(window.localStorage.getItem("Character"))
+      const characterObject = characterList.find(character => character.name === characterName)
+
+      searchResultsContainer.classList.add("inactive")
+      characterDetailsContainer.classList.remove("inactive")
+
+      detailsContainer.innerHTML +=
+      `
+        <div class="character-photo">
+          <img src="${characterObject.image || "assets/user.png"}" alt="character selected photo">
+        </div>
+        <div class="character-principal-details">
+          <span class="details-name">${characterObject.name}</span>
+          <span class="details-birth">${characterObject.dateOfBirth}</span>
+        </div>
+        <div class="character-secondary-details">
+          <div>
+            <span>Alive</span>
+            <p>${characterObject.alive}</p>
+          </div>
+          <div>
+            <span>House</span>
+            <p>${characterObject.house}</p>
+          </div>
+          <div>
+            <span>Species</span>
+            <p>${characterObject.species}</p>
+          </div>
+          <div>
+            <span>Gender</span>
+            <p>${characterObject.gender}</p>
+          </div>
+          <div>
+            <span>Ancestry</span>
+            <p>${characterObject.ancestry}</p>
+          </div>
+          <div>
+            <span>Actor</span>
+            <p>${characterObject.actor}</p>
+          </div>
+        </div>
+      `
+
+      closeDetailsContainer.onclick = () => {
+        searchResultsContainer.classList.remove("inactive")
+        characterDetailsContainer.classList.add("inactive")
+        detailsContainer.innerHTML = ""
+      }
+    }
+  })
+}
+
 
 if (characterButton){
   characterButton.addEventListener("click", async() => {
@@ -70,6 +165,8 @@ if (characterButton){
     window.localStorage.setItem('Character', JSON.stringify(data));
     const charactersHTML = createCharactersHtml(data);
     document.getElementById("results").innerHTML = charactersHTML
+
+    showCharacterDetails()
   })
 }
 
@@ -88,15 +185,6 @@ if (logoButtonNav){
 const sortABtn = document.querySelector('#a-z');
 const sortZBtn = document.querySelector('#z-a');
 
-export function sortingCharacters (charactersList){
-  const newCharactersSorted = charactersList.sort((a, b) => {
-    if(a.name < b.name) { return -1; }
-    if(a.name > b.name) { return 1; }
-    return 0
-  })
-  return newCharactersSorted
-}
-
 if (sortABtn){
   sortABtn.onclick = () => {
     const newCharacters = JSON.parse(window.localStorage.getItem('Character'))
@@ -105,9 +193,11 @@ if (sortABtn){
     if(newCharactersFiltered){
       const charactersHTML = createCharactersHtml(sortingCharacters(newCharactersFiltered))
       document.getElementById("results").innerHTML = charactersHTML
+      showCharacterDetails()
     } else{
       const charactersHTML = createCharactersHtml(sortingCharacters(newCharacters))
       document.getElementById("results").innerHTML = charactersHTML
+      showCharacterDetails()
     }
   }
 }
@@ -120,9 +210,11 @@ if (sortZBtn) {
     if(newCharactersFiltered){
       const charactersHTML = createCharactersHtml(sortingCharacters(newCharactersFiltered).reverse())
       document.getElementById("results").innerHTML = charactersHTML
+      showCharacterDetails()
     } else {
       const charactersHTML = createCharactersHtml(sortingCharacters(newCharacters).reverse())
       document.getElementById("results").innerHTML = charactersHTML
+      showCharacterDetails()
     }
   }
 }
@@ -134,10 +226,8 @@ const filterHufflepuffBtn = document.querySelector('#hufflepuff');
 const filterRavenclawBtn = document.querySelector('#ravenclaw');
 const filterHouseUndefinedBtn = document.querySelector('#houseUndefined');
 
-function filterCharacters (charactersList, filterParam){
-  const newCharactersFiltered = charactersList.filter(character => {
-    return character.house === filterParam
-  })
+function filterCharacters(newCharacters, param){
+  const newCharactersFiltered = filterCharactersFun(newCharacters, param)
   window.localStorage.setItem('CharacterFiltered', JSON.stringify(newCharactersFiltered));
   return newCharactersFiltered
 }
@@ -147,7 +237,7 @@ function setFilterCharacters (filterparam){
   const newCharacters = JSON.parse(window.localStorage.getItem('Character'));
   const filteredCharacters = filterCharacters(newCharacters, param)
   let sortedCharacters = filteredCharacters
-  // console.log(sortABtn.checked, sortZBtn.checked)
+
   if(sortABtn.checked){
     sortedCharacters = sortingCharacters(sortedCharacters)
   }else if(sortZBtn.checked){
@@ -155,6 +245,7 @@ function setFilterCharacters (filterparam){
   }
   const charactersHTML = createCharactersHtml(sortedCharacters);
   document.getElementById("results").innerHTML = charactersHTML
+  showCharacterDetails()
 }
 
 if (filterGryffindorBtn){
@@ -201,3 +292,5 @@ if (filterHouseUndefinedBtn){
     document.getElementById("results").innerHTML = charactersHTML
   }
 }
+
+
